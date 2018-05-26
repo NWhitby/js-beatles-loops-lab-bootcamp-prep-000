e05223609aace3aa7893e98a0a813f0abac24901
NaKia Whitby <whitby.nakia@gmail.com>
2018-05-26 19:22:48 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,8 @@
+function theBeatlesPlay(arrayOfMusicians, arrayOfInstruments){
+let array =[];
+for(let  i = 0; i<arrayMusicians.length; i++){
+
+    array.push(`${arrayMusicians[i]} plays ${arrayInstruments[i]}`);
+}
+  return results;
+}
